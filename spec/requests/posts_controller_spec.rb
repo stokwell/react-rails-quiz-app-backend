d13983a27fe91efb230fef44428c3745a3b52a5b
Vitaly Lyapunov <stokwell1@rambler.ru>
2017-03-12 16:22:53 +0100

    new version of create test

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -33,6 +33,32 @@ RSpec.describe "PostsController", :type => :request  do
 
   end 
 
+  describe 'POST #create' do
+
+    let(:post_params) { FactoryGirl.attributes_for :post }
+
+    context 'when post params are invalid' do
+      before(:each) { post '/api/posts', { post: {nothing: :here} }, headers }
+
+      it 'responds with errors' do
+        json = JSON.parse(response.body)
+        expect(json).to be_truthy
+      end
+      it { expect(response).to have_http_status(422) }
+    end
+
+    context 'when comment params are valid' do
+      before(:each) { post '/api/posts', { post: post_params }, headers }
+
+      it 'creates post and responds with it' do
+      #не знаю как проверить что возвращается тот пост который был создан,
+      # может и не нужно это проверять
+      end
+
+      it { expect(response).to have_http_status(201) }
+    end
+  end
+
   it 'deletes a specific post' do
     post = FactoryGirl.create(:post)  
 
