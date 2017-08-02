d1f0b441910b117beb4fe545afab5b24142e38c8
Vitaly Lyapunov <stokwell1@gmail.com>
2017-08-02 15:29:24 +0200

    now default value false

--- db/migrate/20170802132038_add_right_to_answer.rb
+++ db/migrate/20170802132038_add_right_to_answer.rb
@@ -1,5 +1,5 @@
 class AddRightToAnswer < ActiveRecord::Migration[5.0]
   def change
-    add_column :answers, :right, :boolean, :default => true
+    add_column :answers, :right, :boolean, :default => false
   end
 end
